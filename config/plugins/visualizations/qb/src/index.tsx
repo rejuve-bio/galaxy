8e30fb1f8919dc326d2a9ae36e9b1d724c6f8680
Tibex88 <tibesolomon7@gmail.com>
2024-09-04 15:58:27 +0300

    pass templates from history

--- config/plugins/visualizations/qb/src/index.tsx
+++ config/plugins/visualizations/qb/src/index.tsx
@@ -5,6 +5,8 @@ import App from "./App";
 import { ReactFlowProvider } from "@xyflow/react";
 import "./tailwind.css";
 import "./global.scss";
+import axios from "axios";
+
 
 /* This will be part of the charts/viz standard lib in 23.1 */
 const slashCleanup = /(\/)+/g;
@@ -13,24 +15,33 @@ function prefixedDownloadUrl(root, path) {
 }
 
 window.bundleEntries = window.bundleEntries || {};
-window.bundleEntries.load = function (options) {
+window.bundleEntries.load = async function (options) {
   const dataset = options.dataset;
   const settings = options.chart.settings;
   const explorer = settings.get("explorer");
+
+// get dataset from url
+  async function load(URL){
+    const response = await axios.get(URL);
+    let temp = {};
+    temp["query"] = (response.data);
+    return temp;
+  }
+
   const url =
     window.location.origin +
     prefixedDownloadUrl(options.root,dataset.download_url);
+
+  const temp = await load(url)
+  
   const root = createRoot(document.getElementById(options.target));
     root.render(
       <React.StrictMode>
         <ReactFlowProvider>
-          <App URL={url} />
+          <App template={temp}  />
         </ReactFlowProvider>
       </React.StrictMode>
     );
   options.chart.state("ok", "Chart drawn.");
   options.process.resolve();
 };
-
-
-
